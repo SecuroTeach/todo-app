b123b443dc1b701d87bd27fb5d14f9f182369acd
TheSecurity <jamiedesert01@gmail.com>
2025-02-14 08:14:18 +0100

    Add solution for tutorial 01

---
+++ ToDoApp.Api/Entites/ToDo.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoApp.Api.Entites;
+
+public class ToDo
+{
+    public int Id { get; set; }
+    public required string Content { get; set; }
+}
---
+++ ToDoApp.Api/Models/ToDoUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Api.Models;
+
+public class ToDoUpdateModel
+{
+    [MinLength(5)]
+    public required string Content { get; set; }
+}
